e20bd249ebaae0e5cdc0280760d9ccc2db723201
Just-Pointless <89450999+Just-Pointless@users.noreply.github.com>
2022-12-21 14:42:55 +0800

    added break_eternity.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-    var Points = 0;
-    var ClickMulti = 1;
+    var Points = new Decimal(0);
+    var ClickMulti = new Decimal(1);
     var menu;
-    var ClickMultiPrice = 10;
-    var auto1 = 0;
-    var auto1Price = 30;
-    var allautomulti = 1
+    var ClickMultiPrice = new Decimal(10)
+    var auto1 = new Decimal(0);
+    var auto1Price = new Decimal(30);
+    var allautomulti = new Decimal(1)
     var timeplayed = 0
-    var boosters = 0
-    var UPGClickMulti = 1
+    var booster = new Decimal(0)
+    var UPGClickMulti = new Decimal(1)
     const upgradesbought = [false , false , false]
     menu = switchMenu("main");
-
     // Hiding Buttons
     document.getElementById("BuyBooster").style.display = "none";
+    document.getElementById("UPG_Autoclicker").style.display = "none";
 
     function readable(number) {
     var s = ['', 'k', 'M', 'B', 'T'];
@@ -26,27 +26,40 @@ $(document).ready(function(){
 }   
     function GetInfo(type){
         if (type == "clickamt"){
-            return ClickMulti * Math.pow(2 , boosters) * UPGClickMulti
+            //return ClickMulti * Math.pow(2 , boosters) * UPGClickMulti
+            return ClickMulti.multiply(Decimal.pow(2 , booster)).multiply(UPGClickMulti)
+            //var c1 = 
         }
     }
     setInterval(function(){
-        $("#pps").html("Points Per Second: " + readable(Math.round(auto1 * allautomulti * Math.pow(2 , boosters) * 100) / 100));
-        Points += auto1 / 60 * allautomulti * Math.pow(2 , boosters);
+        $("#pps").html("Points Per Second: " + readable(Math.round(auto1 * allautomulti * Math.pow(2 , booster) * 100) / 100));
+        //Points += auto1 / 60 * allautomulti * Math.pow(2 , boosters);
+        Points = Decimal.add(Points , auto1.divide(30).multiply(Decimal.pow(2 , booster)).multiply(allautomulti))
         $("#points").html("Points: " + readable(Math.round(Points * 100) / 100));
+        //$("#points").html("Points: " + Points)
         console.log("tick")
-    }, 16.666667);
+        //console.log(Points)
+    }, 33.33);
 
+    setInterval(function(){
+
+        //document.getElementById("ClickPoint").disabled = "true";
+        //Points = Decimal.add(Points , GetInfo("clickamt"))
+    }, 100)
     //tick()
     $("#ClickPoint").click(function(){
-        Points += GetInfo("clickamt");
-	    //$("#points").html("Points: " + readable(Math.round(Points * 100) / 100));
+        //document.getElementById("ClickPoint").disabled = "true";
+        Points = Decimal.add(Points , GetInfo("clickamt"))
+        //setTimeout(function() {document.getElementById("ClickPoint").disabled = false;}, 100);
     });
 
     $("#UpgradeClick").click(function(){
-        if (Points >= ClickMultiPrice){
-            Points = Points - ClickMultiPrice;
-            ClickMulti += 1;
-            ClickMultiPrice = ClickMultiPrice + 5 + ClickMulti;
+        console.log()
+        if (Decimal.compare(Points , ClickMultiPrice) >= 0){
+            Points = Decimal.sub(Points , ClickMultiPrice)
+            ClickMulti = ClickMulti.add(1)
+            //ClickMultiPrice = ClickMultiPrice + 5 + ClickMulti;
+            ClickMultiPrice = ClickMultiPrice.plus(5).plus(ClickMulti)
             $("#ClickPoint").html("+" + GetInfo("clickamt") + " Points");
 	        //$("#points").html("Points: " + readable(Math.round(Points * 100) / 100));
             $("#UpgradeClick").html("Increase Click Amount (" + ClickMultiPrice + " Points)");
@@ -54,28 +67,29 @@ $(document).ready(function(){
     });
 
     $("#UpgradeAuto1").click(function(){
-        if (Points >= auto1Price){
-            Points = Points - auto1Price;
-            auto1 += 1;
-            auto1Price = auto1Price + 15 + auto1;
+        if (Decimal.compare(Points , auto1Price) >= 0){
+            Points = Decimal.sub(Points , auto1Price)
+            auto1 = auto1.add(1)
+            auto1Price = auto1Price.add(15).add(auto1)
 	        //$("#points").html("Points: " + readable(Math.round(Points * 100) / 100));
             $("#UpgradeAuto1").html("Increase Auto Points (" + auto1Price + " Points)");
         }
     });
 
     $("#BuyBooster").click(function(){
-        if (Points >= Math.pow(10 ,4 + boosters)){
+        if (Decimal.compare(Points , Decimal.pow(10 , Decimal.add(4 , booster))) >= 0){ // Points >= Math.pow(10 ,4 + booster)
             if (confirm("This will reset everything except upgrades") == true) {
-            Points = 0;
-            ClickMulti = 1;
-            ClickMultiPrice = 10;
-            auto1 = 0;
-            auto1Price = 30; 
+            Points = new Decimal(0);
+            ClickMulti = new Decimal(1);
+            ClickMultiPrice = new Decimal(10);
+            auto1 = new Decimal(0);
+            auto1Price = new Decimal(30); 
             $("#UpgradeAuto1").html("Increase Auto Points (" + auto1Price + " Points)");
             $("#UpgradeClick").html("Increase Click Amount (" + ClickMultiPrice + " Points)");
             $("#ClickPoint").html("+" + GetInfo("clickamt") + " Points");
-            boosters += 1
-            $("#BuyBooster").html("Buy Booster (" + Math.pow(10 ,4 + boosters) + " Points")
+            booster = Decimal.add(booster , 1)
+            $("#BuyBooster").html("Buy Booster (" + Decimal.pow(10 , Decimal.add(4 , booster)) + " Points")
+            document.getElementById("UPG_Autoclicker").style.display = "block";
             }
         }
     });
@@ -95,8 +109,8 @@ $(document).ready(function(){
     });
 
     $("#UPG_DoublePassivePoint").click(function(){  
-        if (Points >= 500 && upgradesbought[0] == false){
-            Points = Points - 500
+        if (Decimal.compare(Points , 500) >= 0 && upgradesbought[0] == false){
+            Points = Decimal.sub(Points , 500)
             upgradesbought[0] = true
             allautomulti = allautomulti * 2
             document.getElementById("UPG_DoublePassivePoint").style.backgroundColor = "lime";
@@ -104,8 +118,8 @@ $(document).ready(function(){
     });
 
     $("#UPG_DoubleClickPoint").click(function(){  
-        if (Points >= 1000 && upgradesbought[1] == false){
-            Points = Points - 1000
+        if (Decimal.compare(Points , 1000) >= 0 && upgradesbought[1] == false){
+            Points = Decimal.sub(Points , 1000)
             UPGClickMulti = UPGClickMulti * 2
             upgradesbought[1] = true
             document.getElementById("UPG_DoubleClickPoint").style.backgroundColor = "lime";
@@ -114,8 +128,8 @@ $(document).ready(function(){
     });
 
     $("#UPG_Booster").click(function(){  
-        if (Points >= 2000 && upgradesbought[2] == false){
-            Points = Points - 2000
+        if (Decimal.compare(Points , 2000) >= 0 && upgradesbought[2] == false){
+            Points = Decimal.sub(Points , 2000)
             upgradesbought[2] = true
             document.getElementById("UPG_Booster").style.backgroundColor = "lime";
             document.getElementById("BuyBooster").style.display = "inline";
